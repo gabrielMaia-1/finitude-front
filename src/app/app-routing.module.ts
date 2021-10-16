07c6a9594cc6a2e30bc6d9db14a46cb4277fa31b
gabrielMaia-1 <gab-maia@outlook.com>
2021-10-16 14:26:39 -0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { AuthGuardService } from './core/auth-guard.service';
 
 const routes: Routes = [
   {path: 'baralho', loadChildren: () => import('./modules/carta/carta.module').then(m => m.CartaModule)},
-  {path: 'adm', loadChildren: () => import('./modules/adm/adm.module').then(m => m.AdmModule), canActivate: [AuthGuardService]}
+  {path: 'adm', loadChildren: () => import('./modules/adm/adm.module').then(m => m.AdmModule), canActivate: [AuthGuardService]},
+  {path: '', pathMatch: 'full', redirectTo: 'adm'}
 ];
 
 @NgModule({
